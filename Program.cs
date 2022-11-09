c56d8fc766ec55c7aeb05a5aab2d599166b66ec9
Ricard211 <ricard.avin@stud.vgtu.lt>
2022-11-09 13:53:40 +0200

    Created all needed files to Start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ricard_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+    
+}
